1ac3061709bcca6171d969ad27da5f0fe15b4b81
Ashu <ashuaman111@gmail.com>
2020-12-26 12:41:05 +0530

    Higher Order FUnctions

--- js_basics/functions/functions.js
+++ js_basics/functions/functions.js
@@ -31,3 +31,27 @@ console.log(area(3,4))
 console.log(area(4))
 // in javascript we can call the function with less arguments because it has dynamic typechecking the arguments which are not defined
 // are considered undefined and we can use this for implemnting polymorphism
+
+
+// higher order functions : 
+// when we can pass a function or return a function inside afunction then it is called a higher order function
+
+function createGreet(greeting) {
+
+    function greet(name) {
+        console.log(greeting,name)
+    }
+
+    return greet
+}
+
+function getName() {
+    return document.getElementById("namebox").value
+}
+
+let g1= createGreet('good morning')
+let g2= createGreet('good evening')
+
+console.log(typeof g1)
+
+
