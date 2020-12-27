6b04c92619c461207e2b412565228397aa166679
Ashu <ashuaman111@gmail.com>
2020-12-27 12:09:05 +0530

    about closure scopes

---
+++ js_basics/closures/concepts.js
@@ -0,0 +1,20 @@
+function outer(arg1) {
+    let var1=10
+    function inner(arg2) {
+        let var2=20
+
+        console.log(arg1,var1,arg2,var2)
+    }
+
+    return inner
+}
+
+let x=outer('param1')
+
+x('param2')
+
+// here we are able to access the arg1 and var1 from outside the function becaues we are calling the inner function by storing it in x 
+// we are able to access these bacause of closure scope 
+// CLOSURE SCOPE :-
+// Where the funtion is born is born all the scope available at the birth place of function is captured by the function this is called 
+// closure scope.
