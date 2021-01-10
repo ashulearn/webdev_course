7ed0169e3352fda04ee87e942efbc503789d6f1d
Ashu <ashuaman111@gmail.com>
2021-01-10 14:46:30 +0530

    express files

--- express/server.js
+++ express/server.js
@@ -7,3 +7,17 @@ app.get('/', (req,res) => {
 app.listen(4444,() => {
     console.log('hello server')
 })
+app.get('/form', (req,res) => {
+    res.sendFile(__dirname+"/files/form.html")
+})
+//dealing with variable in url
+app.get('/:city/welcome',(req,res) => {
+    res.send("welcome to " + req.params.city)
+})
+app.get('/:city/:greeting',(req,res) => {
+    res.send(req.params.greeting +" to " + req.params.city)
+})
+app.get('/:person/:action',(req,res) => {
+    res.send("Thank you " + req.params.person+ "for" + req.params.city)
+})
+//here we have same kind of url for both so it is handled always by 1st one
