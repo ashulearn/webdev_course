f4efb2e387863c312dfd5bd85c165ae8c4b7bf3a
Ashu <ashuaman111@gmail.com>
2021-04-28 16:16:51 +0530

    some code

--- social-media-app/src/controller/users.js
+++ social-media-app/src/controller/users.js
@@ -1,2 +1,26 @@
 const {Users} = require('../db/model')
+const {generateRandomUserName} = require('../utils/username')
 
+async function createAnonUser()
+{
+    const user = await Users.create({
+        name : generateRandomUserName()
+    })
+
+    return user
+}
+
+async function task() 
+{
+    console.log(await createAnonUser())
+    console.log('------------------------')
+    console.log(await createAnonUser())
+    console.log('------------------------')
+    console.log(await createAnonUser())
+    console.log('------------------------')
+    console.log(await createAnonUser())
+    console.log('------------------------')
+
+}
+
+task()
